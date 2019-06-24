7e292e028ad9172258406efa1042640957d5d93a
Anton Durandin <enitefall81@gmail.com>
2019-06-24 13:51:50 -0700

    Add SignIn page

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,29 @@ router.get("/new", (request, response) => {
     response.render("./posts/new");
 });
 
+router.post("/", (request, response) => {
+    knex("posts")
+      .insert({
+        title: request.body.title,
+        imageUrl: request.body.imageUrl,
+        content: request.body.content
+      })
+      .returning("*")
+      .then(posts => {
+        const [post] = posts;
+        response.redirect(`/posts/${post.id}`);
+      });
+  });
+
+  router.get("/", (request, response) => {
+    knex("posts")
+      .orderBy("created_at", "desc")
+      .then(posts => {
+        response.render("posts/index", { posts: posts });
+      });
+  });
+  
+
+  router
+
 module.exports = router;
--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,27 @@ const express = require("express");
 const knex = require("../db/client");
 const router = express.Router();
 
+router.get("/sign_in", (request, response) => {
+
+    const username = request.query.username;
+
+    response.render("signIn",{
+        username: username,
+    });
+});
+
+const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
+router.post("/sign_in", (request, response) => {
+
+    const username = request.body.username;
+    response.cookie("username", username, { maxAge: COOKIE_MAX_AGE});
+
+    response.redirect("/loged_in");
+});
+
+router.get("/loged_in",(request, response) => {
+    response.render("logedIn");
+});
+
+
 module.exports = router;
